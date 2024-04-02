e69e8cd704690b05158945caba593caa5390e68d
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-04-02 07:42:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 function validEmail(str) {
-  //your JS code here.
-}
 
+  // your JS code here.
+	let atTheRate = false;
+	for(int i = 0; i < str.length; i++){
+		if(str.charAt(0) === '@' ) {
+			return false;
+		}
+		if(str.charAt(i) === '@' ) {
+			atTheRate = i;
+		}
+		else if(str.charAt(i) === '.' && atTheRate === 0){
+			return false;
+		} else if(str.charAt(i) === '.' && i-atTheRate-1 < 1){
+			return false;
+		}
+	}
+	return true;                
+}
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
